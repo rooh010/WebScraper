193306c817a4f66364758edffaad48535eebba66
Andrew Fay <andrew.fay@hotmail.co.uk>
2020-08-17 18:56:56 +0100

    working example

--- WebScraper/Form1.Designer.cs
+++ WebScraper/Form1.Designer.cs
@@ -48,6 +48,7 @@
             this.btnStart.TabIndex = 1;
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // Form1
             // 
--- WebScraper/Form1.cs
+++ WebScraper/Form1.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using AngleSharp.Text;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +22,80 @@ namespace WebScraper
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string Title { get; set; }
+        private string Url { get; set; }
+        private string siteUrl = "https://www.oceannetworks.ca/news/stories";
+        public string[] QueryTerms { get; } = { "Ocean", "Nature", "Pollution" };
+
+        internal async void ScrapeWebsite()
+        {
+            CancellationTokenSource cancellationToken = new CancellationTokenSource();
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage request = await httpClient.GetAsync(siteUrl);
+            cancellationToken.Token.ThrowIfCancellationRequested();
+
+            Stream response = await request.Content.ReadAsStreamAsync();
+            cancellationToken.Token.ThrowIfCancellationRequested();
+
+            HtmlParser parser = new HtmlParser();
+            IHtmlDocument document = parser.ParseDocument(response);
+
+            //Add connection between initial scrape, and parsing of results
+            GetScrapeResults(document);
+        }
+
+        private void GetScrapeResults(IHtmlDocument document)
+        {
+            IEnumerable<IElement> articleLink = null;
+
+            foreach (var term in QueryTerms)
+            {
+                articleLink = document.All.Where(x =>
+                    x.ClassName == "views-field views-field-nothing" &&
+                    (x.ParentElement.InnerHtml.Contains(term) || x.ParentElement.InnerHtml.Contains(term.ToLower()))).Skip(1);
+
+                //Overwriting articleLink above means we have to print it's result for all QueryTerms
+                //Appending to a pre-declared IEnumerable (like a List), could mean taking this out of the main loop.
+                if (articleLink.Any())
+                {
+                    PrintResults(articleLink);
+                }
+            }
+        }
+
+        public void PrintResults(IEnumerable<IElement> articleLink)
+        {
+            //Every element needs to be cleaned and displayed
+            foreach (var element in articleLink)
+            {
+                CleanUpResults(element);
+
+                richTextBox1.AppendText($"{Title} - {Url}{Environment.NewLine}");
+            }
+        }
+
+        private void CleanUpResults(IElement result)
+        {
+            string htmlResult = result.InnerHtml.ReplaceFirst("        <span class=\"field-content\"><div><a href=\"", @"https://www.oceannetworks.ca");
+            htmlResult = htmlResult.ReplaceFirst("\">", "*");
+            htmlResult = htmlResult.ReplaceFirst("</a></div>\n<div class=\"article-title-top\">", "-");
+            htmlResult = htmlResult.ReplaceFirst("</div>\n<hr></span>  ", "");
+
+            //Seperate the results into our class fields for use in PrintResults()
+            SplitResults(htmlResult);
+        }
+
+        private void SplitResults(string htmlResult)
+        {
+            string[] splitResults = htmlResult.Split('*');
+            Url = splitResults[0];
+            Title = splitResults[1];
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            ScrapeWebsite();
         }
     }
 }
